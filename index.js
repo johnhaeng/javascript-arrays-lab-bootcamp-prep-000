17782910c760aadeb58d6931b91c96471f8fce57
inherited-firmware-6870 <johnhaeng@gmail.com>
2018-11-21 19:48:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var kittens = [`Milo`, `Otis`, `Garfield`]; //define your array here
 
-kittens.concat(destructivelyAppendKitten())
+var destructivelyAppendKitten = [`Somyeong`];
+
+kittens.concat(destructivelyAppendKitten);
 // Add your functions and code here
